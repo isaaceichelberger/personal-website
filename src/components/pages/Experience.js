7c577ef3650349e5d41383fe2d479db09afee1bd
Isaac Eichelberger <Isaac.Eichelberger@ibm.com>
2022-12-23 21:38:53 -0600

    experience tiles done and responsive

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,10 +1,38 @@
 import React from "react";
+import ResponsiveDiv from "../wrappers/ResponsiveDiv";
+import ibm from "../../images/ibm.jpeg";
+import cfa from "../../images/cfa.jpeg";
+import ExperienceTile from "../tiles/ExperienceTile";
 
 const Experience = () => {
   return (
     <div id="experience">
       <div className="experienceLanding">
-        <div className="heading">Experience</div>
+        <ResponsiveDiv className="heading">Experience</ResponsiveDiv>
+        <ResponsiveDiv className="experienceContainer">
+          <ExperienceTile
+            src={ibm}
+            alt="IBM2"
+            aria="ibm2"
+            description="As a backend developer, I worked on the Space Tech team. I created an open source project called PlasticNet, which detects different types of plastic waste in the ocean using computer vision."
+            label="Backend Developer Intern"
+          ></ExperienceTile>
+          <ExperienceTile
+            src={ibm}
+            alt="IBM"
+            aria="ibm"
+            description="I currently work as a solutions developer for IBM, with the IBM Cloud Solutions Engineering Team. I demonstrate cloud technologies to clients and build tools for them."
+            label="Solution Developer"
+            active
+          ></ExperienceTile>
+          <ExperienceTile
+            src={cfa}
+            alt="CFA"
+            aria="cfa"
+            description="At Chick-Fil-A, I improved Chick-Fil-A's forecasting system by implementing weather forecasts into the store's inventory forecast, as well as automated internal processes for store grand openings."
+            label="Operator Technology Solutions Intern"
+          ></ExperienceTile>
+        </ResponsiveDiv>
       </div>
     </div>
   );
--- src/components/tiles/ExperienceTile.js
+++ src/components/tiles/ExperienceTile.js
@@ -1,15 +1,46 @@
 import React from "react";
+import ResponsiveDiv from "../wrappers/ResponsiveDiv";
+import "./experienceTile.css";
+import MediaQuery from "react-responsive";
 
 class ExperienceTile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: false,
+      active: this.props.active,
     };
   }
 
   render() {
-    return <></>;
+    return (
+      <ResponsiveDiv
+        className={"container" + (this.state.active ? "" : " inactive")}
+      >
+        <MediaQuery maxWidth="768px">
+          {(matches) =>
+            matches ? (
+              <img
+                alt={this.props.alt}
+                src={this.props.src}
+                aria-label={this.props.aria}
+                className={"tile mobile"}
+              />
+            ) : (
+              <img
+                alt={this.props.alt}
+                src={this.props.src}
+                aria-label={this.props.aria}
+                className={"tile"}
+              />
+            )
+          }
+        </MediaQuery>
+        <ResponsiveDiv className={"label"}>{this.props.label}</ResponsiveDiv>
+        <ResponsiveDiv className={"description"}>
+          {this.props.description}
+        </ResponsiveDiv>
+      </ResponsiveDiv>
+    );
   }
 }
 
